7d132b134c705bc53f5845199934a732508dc500
brian kirkby <bkirkby@kirkby.org>
2015-11-03 21:43:40 -0800

    added mouse commands for move and press-and-hold. added explosion effect on mouseup

--- boids.js
+++ boids.js
@@ -1,29 +1,22 @@
 /* Boid prototype */
 
-function Boid(swarm) {
-    this.x = Math.random() * swarm.width;
-    this.y = Math.random() * swarm.height;
+function Boid(swarm, x, y, color, radius ) {
+    this.x = x;
+    this.y = y;
     this.heading = Math.random() * 2 * Math.PI - Math.PI;
+    this.color = color;
+    this.radius = radius;
 }
 
-Boid.prototype.radius = 6;
-Boid.prototype.speed = 2;
-Boid.prototype.radialSpeed = Math.PI / 60;
-Boid.prototype.vision = 50;
+Boid.prototype.radialSpeed = Math.PI / 15;
+Boid.prototype.vision = 100;
+Boid.prototype.speed = 12;
 
 Boid.prototype.draw = function(ctx) {
-    var pointLen = this.radius * 2.5;
-    ctx.fillStyle = 'blue';
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.beginPath();
-    ctx.moveTo(this.x + Math.cos(this.heading + Math.PI / 2) * this.radius,
-               this.y + Math.sin(this.heading + Math.PI / 2) * this.radius);
-    ctx.lineTo(this.x + Math.cos(this.heading + Math.PI) * pointLen,
-               this.y + Math.sin(this.heading + Math.PI) * pointLen);
-    ctx.lineTo(this.x + Math.cos(this.heading - Math.PI / 2) * this.radius,
-               this.y + Math.sin(this.heading - Math.PI / 2) * this.radius);
+    ctx.globalAlpha=1;
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, 1.9 * Math.PI);
     ctx.fill();
 };
 
@@ -78,6 +71,32 @@ Boid.meanAngle = function() {
 
 Boid.prototype.step = function(swarm) {
     var w = swarm.width, h = swarm.height;
+    //check for attractor
+    if( swarm.attractor != null) {
+      target = Math.atan2(swarm.predator.y - this.y, swarm.predator.x - this.x);
+      // Move in this direction
+      var delta = Boid.wrap(target - this.heading, -Math.PI, Math.PI);
+      delta = Boid.clamp(delta, this.radialSpeed);
+      this.heading = Boid.wrap(this.heading + delta, -Math.PI, Math.PI);
+      this.move(swarm);
+      return;
+    }
+
+    //check for predator
+    if( swarm.predator != null) {
+      //if predator in visual range
+      if( this.distance( swarm.predator, w, h) < this.vision) {
+        target = Math.atan2(this.y - swarm.predator.y, this.x - swarm.predator.x);
+        // Move in this direction
+        var delta = Boid.wrap(target - this.heading, -Math.PI, Math.PI);
+        delta = Boid.clamp(delta, this.radialSpeed);
+        this.heading = Boid.wrap(this.heading + delta, -Math.PI, Math.PI);
+        this.move(swarm);
+        return;
+      }
+    }
+
+    //check for neighbors
     var neighbors = this.getNeighbors(swarm);
     if (neighbors.length > 0) {
         var meanhx = 0, meanhy = 0;
@@ -123,27 +142,136 @@ Boid.prototype.step = function(swarm) {
 Boid.prototype.move = function(swarm) {
     var padding = swarm.padding;
     var width = swarm.width, height = swarm.height;
-    this.x = Boid.wrap(this.x + Math.cos(this.heading) * this.speed,
+    var speed = this.speed;
+    this.x = Boid.wrap(this.x + Math.cos(this.heading) * speed,
                        -padding, width + padding * 2);
-    this.y = Boid.wrap(this.y + Math.sin(this.heading) * this.speed,
+    this.y = Boid.wrap(this.y + Math.sin(this.heading) * speed,
                        -padding, height + padding * 2);
 };
+
+/* Predator prototype */
+
+function Predator( sx, sy) {
+  this.x = sx;
+  this.y = sy;
+  this.updateLoc = function( x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+/* Attractor prototype */
+
+function Attractor( sx, sy) {
+  this.x = sx;
+  this.y = sy;
+  this.updateLoc = function( x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+/* DestroyBit prototype */
+
+var destroyBitsFactor = 3.0; //percent of boid.radius to create when destroyed
+var destroySpeedMin = 5;
+var destroySpeedMax = 8;
+var destroyDistanceMin = 30;
+var destroyDistanceMax = 52;
+
+function DestroyBit( boid){ 
+  this.size = Math.random()>.4?1:2;
+  this.swarm = boid.swarm;
+  this.originX = boid.x;
+  this.originY = boid.y;
+  this.x = boid.x;
+  this.y = boid.y;
+  this.speed = Math.floor( (Math.random() * destroySpeedMax) + destroySpeedMin);
+  this.lifeDistance = Math.floor( (Math.random() * destroyDistanceMax) + destroyDistanceMin);
+  this.heading = Math.random() * 2 * Math.PI - Math.PI;
+  this.color = boid.color;
+  this.move = function( swarm) {
+    //check distance. if past this.lifeDistance, then return false to destroy bit
+    if( Math.sqrt( Math.pow((this.originX-this.x),2) + Math.pow((this.originY-this.y),2)) >= this.lifeDistance) {
+      return false;
+    }
+    var width = swarm.width, height = swarm.height;
+    var speed = this.speed;
+    this.x = this.x + Math.cos(this.heading) * speed;
+    this.y = this.y + Math.sin(this.heading) * speed;
+    return true;
+  }
+  this.draw = function( ctx) {
+    console.log( "destroyBit: "+this.x+","+this.y);
+    ctx.fillStyle = this.color;
+    ctx.fillRect( this.x, this.y, this.size, this.size);
+  }
+}
+
 
 /* Swam prototype. */
 
 function Swarm(ctx) {
-    this.ctx = ctx;
-    this.boids = [];
-    var swarm = this;
-    this.animate = function() {
-        Swarm.step(swarm);
-    };
-    this.padding = 8;
+  this.ctx = ctx;
+  this.boids = [];
+  this.destroyBits = [];
+  var swarm = this;
+  this.predator = null;
+  this.attractor = null;
+  this.padding = 4;
+
+  //functions
+  this.animate = function() {
+      Swarm.step(swarm);
+  };
+  this.updatePredatorLoc = function( x, y) {
+    if( this.predator == null) {
+      this.predator = new Predator( x, y);
+    } else {
+      this.predator.updateLoc( x, y);
+    }
+  };
+  this.updateAttractorLoc = function( x, y) {
+    if( this.attractor == null) {
+      this.attractor = new Attractor( x, y);
+    } else {
+      this.attractor.updateLoc( x, y);
+    }
+  };
+  this.setDestructionAtPoint = function( x, y) {
+    for(var i=this.boids.length-1; i>=0; i--){
+      if( Math.abs(this.boids[i].x-x) < this.boids[i].radius && Math.abs(this.boids[i].y-y) < this.boids[i].radius) {
+        this.setDestruction( this.boids[i]);
+        this.boids.splice( i, 1);
+      }
+    }
+  };
+  this.setDestruction = function( boid) {
+    for( var numBits = Math.floor(boid.radius*destroyBitsFactor); numBits>=0; numBits--) {
+      this.destroyBits.push( new DestroyBit( boid));
+    }
+  };
 }
 
-Swarm.prototype.createBoid = function(n) {
-    for (var i = 0; i < (n || 1); i++) {
-        this.boids.push(new Boid(this));
+
+Swarm.prototype.addPersonal = function() {
+  for( var i = 0; i < this.boids.length; i++ ) {
+    if( this.boids[i].isPersonal) {
+      return;
+    }
+  }
+  //this.predator = new Predator( swarm, this.width/2, this.height/2);
+  this.boids.push(new Boid(this, this.width/2, this.height/2, '#00fcff', 11));
+}
+
+Swarm.prototype.createBoids = function(n) {
+    //reduce number depending on area
+    /*var idealArea = 600*1150;
+    if( this.width * this.height < idealArea) {
+      n = Math.floor(n * ((this.width*this.height)/idealArea));
+    }*/
+    for (var i = 0; i < n; i++) {
+        this.boids.push(new Boid(this, this.width/2, this.height/2, '#006a6b', 9));
     }
 };
 
@@ -159,28 +287,84 @@ Object.defineProperty(Swarm.prototype, 'height', {get: function() {
     return this.ctx.canvas.height;
 }});
 
+Swarm.prototype.getZPath = function( ctx) {
+  ctx.beginPath();
+  ctx.moveTo( 565, 210);
+  ctx.lineTo( 565, 80);
+  ctx.lineTo( 870, 80);
+  ctx.arcTo( 950, 78, 890, 182, 50);
+  ctx.lineTo( 667, 606);
+  ctx.lineTo( 920, 606);
+  ctx.lineTo( 920, 665);
+  ctx.lineTo( 605, 665);
+  ctx.arcTo( 558, 665, 558, 600, 50);
+  ctx.lineTo( 558, 605);
+  ctx.lineTo( 800, 132);
+  ctx.lineTo( 653, 132);
+  ctx.arcTo( 603, 132, 603, 200, 50);
+  ctx.lineTo( 603, 210);
+  ctx.closePath();
+};
+
 Swarm.step = function (swarm) {
     var ctx = swarm.ctx;
-    if (ctx.canvas.width != window.innerWidth)
-        ctx.canvas.width = window.innerWidth;
-    if (ctx.canvas.height != window.innerHeight)
-        ctx.canvas.height = window.innerHeight;
-    ctx.fillStyle = 'white';
+    ctx.canvas.width = ctx.canvas.parentElement.clientWidth;
+    ctx.canvas.height = ctx.canvas.parentElement.clientHeight;
+    ctx.fillStyle = '#050505';
     ctx.fillRect(0, 0, swarm.width, swarm.height);
 
     for (var i = 0; i < swarm.boids.length; i++) {
         swarm.boids[i].step(swarm);
         swarm.boids[i].draw(ctx);
     }
+
+    for (var i = swarm.destroyBits.length-1; i >= 0; i--) {
+      if( !swarm.destroyBits[i].move( swarm)) {
+        swarm.destroyBits.splice( i, 1);
+      } else {
+        swarm.destroyBits[i].draw(ctx);
+      }
+    }
 };
 
-/* Test */
+Swarm.prototype.placePatternedBoids = function( num) {
+  var ctx = this.ctx;
+  this.getZPath( ctx);
+  var xmin=565;
+  var xmax=920;
+  var ymin=80;
+  var ymax=665;
+  for( var i=0; i<num; i++) {
+    var x = Math.floor((Math.random() * (xmax-xmin))+xmin);
+    var y = Math.floor((Math.random() * (ymax-ymin))+ymin);
+    while( !ctx.isPointInPath( x, y)) {
+      x = Math.floor((Math.random() * (xmax-xmin))+xmin);
+      y = Math.floor((Math.random() * (ymax-ymin))+ymin);
+    }
+    this.boids.push( new Boid( this, x, y));
+  }
+};
 
-var swarm; // defined globally for skewer
-$(document).ready(function() {
-    swarm = new Swarm($('#canvas').get(0).getContext('2d'));
-    swarm.id = setInterval(swarm.animate, 33);
+var swarm;
+$(document).ready( function() {
+    var canvas = document.getElementById('boidCanvas');
+    swarm = new Swarm( canvas.getContext('2d'));
+    swarm.id = setInterval( swarm.animate, 33);
     swarm.animate();
     swarm.clear();
-    swarm.createBoid(200);
+
+    $("#boidCanvas").on("mousemove", function(e) {
+      swarm.updatePredatorLoc( e.offsetX, e.offsetY);
+    });
+    $("#boidCanvas").on("mouseout", function() {
+      swarm.predator = null;
+      swarm.attractor = null;
+    });
+    $("#boidCanvas").on("mousedown", function(e) {
+      swarm.updateAttractorLoc( e.offsetX, e.offsetY);
+    });
+    $("#boidCanvas").on("mouseup", function(e) {
+      swarm.attractor = null;
+      swarm.setDestructionAtPoint( e.offsetX, e.offsetY);
+    });
 });
